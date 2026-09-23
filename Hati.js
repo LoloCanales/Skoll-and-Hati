20236e91f4d87043e1695e9e1aa42685c1b9342c
Manuel <manucanalesguti@gmail.com>
2026-09-23 15:52:50 +0200

    Refactor functions and improve WebSocket handling

--- Hati.js
+++ Hati.js
@@ -9,7 +9,7 @@ const logMensajes = document.getElementById("log-mensajes");
 function txtPU(texto){
 }
 
-function txtPU(texto) {
+function imprimirConsola(texto) {
 	logMensajes.innerHTML += `<div class="log-entrada">${texto}</div>`;
 	logMensajes.scrollTop = logMensajes.scrollHeight; // Hace scroll hacia abajo
 	}
@@ -31,31 +31,30 @@ const Grafo = ForceGraph3D()
 
 
 // 2. Nos conectamos al cerebro en Python
-        const conexionLocal = new WebSocket("ws://localhost:8765");
+const conexionLocal = new WebSocket("ws://localhost:8765");
 
-        conexionLocal.onopen = () => {
-            logMensajes.innerHTML = ""; // Limpiamos el mensaje de "esperando"
-            imprimirConsola(`<span class="sistema">✅ Conexión segura establecida (127.0.0.1)</span>`);
-        };
+conexionLocal.onopen = () => {
+logMensajes.innerHTML = ""; // Limpiamos el mensaje de "esperando"
+imprimirConsola(`<span class="sistema">✅ Conexión segura establecida (127.0.0.1)</span>`);
+};
 
 
 // 3. El cerebro del sistema visual: ¿Qué hacer al recibir un evento?
-        conexionLocal.onmessage = function(evento) {
-            const datos = JSON.parse(evento.data);
-
-            if (datos.accion === "nuevo_nodo") {
-                imprimirConsola(`[+] <b>Nuevo Agente en red:</b> ${datos.nombre}`);
-                datosGrafo.nodes.push({ id: datos.id, nombre: datos.nombre, grupo: datos.grupo });
-                Grafo.graphData(datosGrafo);
-            } 
-            else if (datos.accion === "nueva_conexion") {
-                imprimirConsola(`[⚡] <b>Enlace establecido:</b> ${datos.origen} ➔ ${datos.destino}`);
-                datosGrafo.links.push({ source: datos.origen, target: datos.destino });
-                Grafo.graphData(datosGrafo);
-            }
-            // ¡NUEVO EVENTO! Para cuando integres IA real y los agentes hablen
-            else if (datos.accion === "mensaje") {
-                imprimirConsola(`🗣️ <span class="agente-nombre">${datos.emisor}:</span> ${datos.texto}`);
-            }
-        };
+conexionLocal.onmessage = function(evento) {
+	const datos = JSON.parse(evento.data);
+	if (datos.accion === "nuevo_nodo") {
+		imprimirConsola(`[+] <b>Nuevo Agente en red:</b> ${datos.nombre}`);
+		datosGrafo.nodes.push({ id: datos.id, nombre: datos.nombre, grupo: datos.grupo });
+		Grafo.graphData(datosGrafo);
+    } 
+	else if (datos.accion === "nueva_conexion") {
+		imprimirConsola(`[⚡] <b>Enlace establecido:</b> ${datos.origen} ➔ ${datos.destino}`);
+		datosGrafo.links.push({ source: datos.origen, target: datos.destino });
+		Grafo.graphData(datosGrafo);
+	}
+// ¡NUEVO EVENTO! Para cuando integres IA real y los agentes hablen
+	else if (datos.accion === "mensaje") {
+		imprimirConsola(`🗣️ <span class="agente-nombre">${datos.emisor}:</span> ${datos.texto}`);
+    }
+};
 
